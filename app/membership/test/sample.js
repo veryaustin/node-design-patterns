6f85b5a2a7e3e0bdb9ffa9f9763edfb460a7c3d2
Austin Lauritsen <austin@veryaustin.com>
2017-03-30 10:47:30 -0500

    Re-organize modules and setup mocha testing

---
+++ app/membership/test/sample.js
@@ -0,0 +1,9 @@
+var assert = require("assert");
+
+describe("Something", function() {
+  describe("something", function() {
+    it("is assertions", function() {
+      assert.ok(true);
+    });
+  });
+});
